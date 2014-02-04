8e68e6b66825689549e83015d88571daec20ace2
Helmut Eller <eller.helmut@gmail.com>
2014-02-04 12:52:10 +0100

    Fix wording in license.
    * Makefile, cl-ns.el, namespace-test.el, namespace-tools.el,
    * namespace.el:

--- namespace-test.el
+++ namespace-test.el
@@ -4,12 +4,12 @@
 
 ;; Author: Helmut Eller <eller.helmut@gmail.com>
 
-;; This file is free software: you can redistribute it and/or modify
+;; This work is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; This file is distributed in the hope that it will be useful,
+;; This work is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
