7016fa4a4f63b65c9b33e479bc2a94af13da4969
Helmut Eller <eller.helmut@gmail.com>
2020-05-10 10:40:02 +0200

    Use SPDX-License-Identifiers

--- namespace-test.el
+++ namespace-test.el
@@ -1,21 +1,9 @@
 ;; namespace-test.el --- tests for namespace.el         -*-lexical-binding:t-*-
 
 ;; Copyright (C) 2014 Helmut Eller
+;; SPDX-License-Identifier: GPL-3.0-or-later
 
 ;; Author: Helmut Eller <eller.helmut@gmail.com>
-
-;; This work is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This work is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
